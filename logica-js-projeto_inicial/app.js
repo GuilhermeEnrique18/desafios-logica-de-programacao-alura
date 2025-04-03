17b52680fab5203a5565886ff53047e51ad3a300
GuilhermeDev <enriqueguilherme1209@gmail.com>
2025-04-03 16:35:45 -0300

    Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,13 +1,2 @@
-//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
-
-alert("Bem vindo ao programa contagem progressiva!");
-
-let contador = 0;
-let contadorUsuario= prompt("Informe um número para contagem: ");
-
-while(contador <= contadorUsuario){
-    console.log(`valor = ${contador}`);
-    contador++
-  
-}
-
+//Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+console.log("Boas vindas!");
