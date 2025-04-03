9c2f92ede682b67d393ed62415e83a8a3ea9ca0a
GuilhermeDev <enriqueguilherme1209@gmail.com>
2025-04-03 13:54:28 -0300

    exercicio contador que incrementa em ordem crescente de 1 a 10

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+//Declaração contador
+let contador = 1
+//while(enquanto) + definição da condição do contador 
+while(contador <=10){
+    console.log(`valor = ${contador}`)
+    contador++
+    //contador++ é a mesma coisa que contador = contador + 1
+}
+
