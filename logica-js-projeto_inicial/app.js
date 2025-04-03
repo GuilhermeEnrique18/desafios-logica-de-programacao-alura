dddb5ea25f3fcce0022be576e241501ed1bfb1ff
GuilhermeDev <enriqueguilherme1209@gmail.com>
2025-04-03 14:13:26 -0300

    exercicio contador que decrementa em ordem decrescente de 10 a 0

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,11 @@
-//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
 
 //Declaração contador
-let contador = 1
+let contador = 10
 //while(enquanto) + definição da condição do contador 
-while(contador <=10){
+while(contador >=0){
     console.log(`valor = ${contador}`)
-    contador++
-    //contador++ é a mesma coisa que contador = contador + 1
+    contador--
+    //contador-- é a mesma coisa que contador = contador - 1
 }
 
