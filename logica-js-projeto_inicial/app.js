e8098d639b2fdd3e97dbdeccb80feffc4321360a
GuilhermeDev <enriqueguilherme1209@gmail.com>
2025-04-03 14:30:40 -0300

    contagem progressiva iniciando do zero e indo ate o numero definido pelo usuario

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,13 @@
-//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
 
-alert("Bem vindo ao programa contagem regressiva!");
+alert("Bem vindo ao programa contagem progressiva!");
 
-let contador = prompt("Informe um número para contagem: ");
+let contador = 0;
+let contadorUsuario= prompt("Informe um número para contagem: ");
 
-while(contador >=0){
+while(contador <= contadorUsuario){
     console.log(`valor = ${contador}`);
-    contador--
+    contador++
   
 }
 
